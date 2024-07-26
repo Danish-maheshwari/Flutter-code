dfa9e34818fc6c3be9c39516b41c7c465edf8b0d
Danish-maheshwari <thedanishmaheshwari@gmail.com>
2024-07-26 15:33:23 +0500

    List Task

---
+++ bin/list_assignment_05.dart
@@ -0,0 +1,21 @@
+void main() {
+  //Q1:
+  List<String> name = ["Danish", "kumar", "Maheshwari"];
+  print(name);
+  // Q2:
+  List<String> days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "sudnay"
+  ];
+  print(days[6]);
+  // Q3:
+  List student_data = ["danish", "8th semseter", 216, 'B', 2.6];
+  print(student_data);
+}
+
+
---
+++ test/list_assignment_05_test.dart
@@ -0,0 +1,8 @@
+import 'package:list_assignment_05/list_assignment_05.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
